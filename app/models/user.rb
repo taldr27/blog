7c561a2b9676b8ef4b0cd9b6174c70f59ff24224
lana <diegogarciaq98@gmail.com>
2023-01-26 10:06:45 -0500

    Add rspec and generate folder

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: :author_id
   has_many :likes, foreign_key: :author_id
   has_many :posts, foreign_key: :author_id
-  validates :name, presence: true
+  validates_presence_of :name
   validates :posts_counter, length: { minimum: 0 }, numericality: { only_integer: true }
   def latest_posts
     posts.order(created_at: :desc).limit(3)
