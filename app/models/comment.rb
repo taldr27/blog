5dac342bb116542b898d2226b7bc034e133c9da3
lana <diegogarciaq98@gmail.com>
2023-01-26 11:57:54 -0500

    Add test for likes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :author, foreign_key: 'author_id', class_name: 'User'
-  belongs_to :post, foreign_key: 'posts_id', class_name: 'Post'
+  belongs_to :post, foreign_key: 'post_id', class_name: 'Post'
 
   def update_comments_counter
     post.increment(:comments_counter)
