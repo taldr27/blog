8ad6c43f485f7e7bf3dde0abdd910bc7fb98b5ee
lana <diegogarciaq98@gmail.com>
2023-01-30 11:11:29 -0500

    Add routes with RESTful

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get '/users' => 'users#index'
-  get '/users/:id', to: 'users#show'
+  # get '/users' => 'users#index'
+  # get '/users/:id', to: 'users#show'
   get '/users/:user_id/posts', to: 'posts#index'
   get '/users/:user_id/posts/:id', to: 'posts#show'
 
-  # resources :users, only: [:index, :show]
-  # resources :posts, only: [:index, :show]
+  resources :users, only: [:index, :show]
 end
