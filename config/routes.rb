5bc10f5353ed8e4ac9dacc00475951738f3d05ae
lana <diegogarciaq98@gmail.com>
2023-01-30 11:46:08 -0500

    Add requested changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # get '/users' => 'users#index'
-  # get '/users/:id', to: 'users#show'
   get '/users/:user_id/posts', to: 'posts#index'
   get '/users/:user_id/posts/:id', to: 'posts#show'
 
