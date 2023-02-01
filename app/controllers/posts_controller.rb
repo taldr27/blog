8402767ef30ce739c7ceaa2f3ba1df2a34666204
lana <diegogarciaq98@gmail.com>
2023-02-01 12:33:22 -0500

    Add view for user index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,9 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @user = User.find(params[:user_id])
+    @latest_posts = @user.latest_posts
+    @posts = Post.find(params[:user_id])
+    @latest_comments = @posts.recent_comments
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/users/:user_id/posts', to: 'posts#index'
+  get '/users/:user_id/posts', to: 'posts#index', as: 'post_path'
   get '/users/:user_id/posts/:id', to: 'posts#show'
 
   resources :users, only: [:index, :show]
