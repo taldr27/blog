a9b91c72ccb9a6afda3a56c13f4f1eeac854fa2d
lana <diegogarciaq98@gmail.com>
2023-02-02 13:22:40 -0500

    Add form for adding posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,32 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @latest_posts = @user.latest_posts
+    @posts = Post.where(author_id: params[:user_id])
     @comments = Comment.all
   end
+  
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.author_id = current_user
+      if @post.save!
+        flash.now[:notice] = 'Comment created successfully'
+        redirect_to user_path(current_user)
+      else
+        redirect_to new
+      end
+  end 
 
   def show
     @post = Post.find(params[:id])
     @user = User.find(params[:user_id])
     @comments = Comment.all
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
