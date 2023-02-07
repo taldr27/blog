6c09aa19977792c42d7b9dba2bae407bfaf4d2bd
lana <diegogarciaq98@gmail.com>
2023-02-07 11:07:28 -0500

    Generate devise User

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,2 @@
 class ApplicationController < ActionController::Base
-  def current_user
-    User.first.id
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get '/users/:user_id/posts', to: 'posts#index', as: 'post_path'
   get '/users/:user_id/posts/:id', to: 'posts#show', as: 'post_show'
   post '/users/:user_id/posts/:id/comments', to: 'comments#create', as: 'comment_create'
