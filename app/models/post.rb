9979f273959d029d0d99eecd6cd37de489bd52e8
lana <diegogarciaq98@gmail.com>
2023-02-08 15:35:50 -0500

    Fix rubocop errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
-  has_many :likes,  dependent: :destroy
+  has_many :likes, dependent: :destroy
   belongs_to :user, foreign_key: 'author_id', class_name: 'User'
   validates :title, length: { maximum: 250 }
   validates :comments_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
@@ -8,7 +8,7 @@ class Post < ApplicationRecord
   after_create :update_posts_counter
   after_destroy :update_posts_counter
   def update_posts_counter
-    user.update(:posts_counter => user.posts.count)
+    user.update(posts_counter: user.posts.count)
   end
 
   def recent_comments
