36644bbe1ad2edec9db564df889c97a923d20f66
lana <diegogarciaq98@gmail.com>
2023-02-08 20:22:27 -0500

    Fix method

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,10 @@ class User < ApplicationRecord
   validates_presence_of :name
   validates :posts_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
 
+  def is?(requested_role)
+    role == requested_role.to_s
+  end
+
   def latest_posts
     posts.order(created_at: :desc).limit(3)
   end
